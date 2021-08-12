5451769c0a947c6f8310c642941dba41bbcc5862
Eungyeol41 <iniziato@naver.com>
2021-08-12 22:16:39 +0900

    수정

--- Main/app/src/main/java/com/ini/main/MainActivity.java
+++ Main/app/src/main/java/com/ini/main/MainActivity.java
@@ -18,22 +18,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale);
+        final Animation animation_du4 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_1);
+        final Animation animation_du2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_2);
+        final Animation animation_x5 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_3);
         final Animation animation_du3 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.test);
 
         View view01 = findViewById(R.id.plus1);
-        view01.startAnimation(animation_du3);
+        view01.startAnimation(animation);
 
         View view02 = findViewById(R.id.plus2);
-        view02.startAnimation(animation);
+        view02.startAnimation(animation_du4);
 
         View view03 = findViewById(R.id.plus3);
-        view03.startAnimation(animation_du3);
+        view03.startAnimation(animation_du2);
 
         View view04 = findViewById(R.id.plus4);
-        view04.startAnimation(animation);
+        view04.startAnimation(animation_du3);
 
         View view05 = findViewById(R.id.plus5);
-        view05.startAnimation(animation);
+        view05.startAnimation(animation_x5);
 
 
     }
