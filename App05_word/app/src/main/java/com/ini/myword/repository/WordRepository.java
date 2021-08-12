fce68d9795229f3ee3b7a3c07fc8df2ce3c73851
Eungyeol41 <iniziato@naver.com>
2021-08-12 11:50:18 +0900

    Word

---
+++ App05_word/app/src/main/java/com/ini/myword/repository/WordRepository.java
@@ -0,0 +1,49 @@
+package com.ini.myword.repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.ini.myword.dao.WordDao;
+import com.ini.myword.model.WordDTO;
+
+import java.util.List;
+
+/*
+ * WordDao와 WordDTO를 사용하여 DataBase Handling을 하는 중간 Service Class
+ */
+public class WordRepository {
+
+    protected WordDao wordDao;
+    protected LiveData<List<WordDTO>> wordList;
+
+    /*
+     * Application Context를 매개변수로 갖는 생성자 필요
+     *
+     * @param application
+     */
+    public WordRepository(Application application) {
+
+    }
+
+    /*
+     * wordList를 단순히 return하고 있는 데 wordList 데이터를 가져오는 코드는 Where...??
+     *
+     * 외부에서 repository.selectAll() method를 호출하면
+     * 내부에서 자동으로 Dao를 거쳐 DB에 Select를 수행하고 그 결과를 return한다
+     *
+     * @return
+     */
+    public LiveData<List<WordDTO>> selectAll() {
+        return wordList;
+    }
+
+    public void insert(WordDTO wordDTO) {
+        wordDao.insert(wordDTO);
+    }
+
+    public void deleteAll() {
+        wordDao.deleteAll();
+    }
+
+}
