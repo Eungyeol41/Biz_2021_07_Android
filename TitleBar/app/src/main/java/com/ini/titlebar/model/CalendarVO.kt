4c34ad3fd9a73b09b5645f4b1c98990a430788a7
Eungyeol41 <iniziato@naver.com>
2022-01-01 02:08:08 +0900

    주간 캘린더 추가

---
+++ TitleBar/app/src/main/java/com/ini/titlebar/model/CalendarVO.kt
@@ -0,0 +1,6 @@
+package com.ini.titlebar.model
+
+data class CalendarVO(
+    var cl_date: String = "", // 날짜
+    var cl_day: String = "" // 요일
+)
--- TitleBar/app/src/main/java/com/ini/titlebar/ui/calendar/CalendarFragment.kt
+++ TitleBar/app/src/main/java/com/ini/titlebar/ui/calendar/CalendarFragment.kt
@@ -1,19 +1,33 @@
 package com.ini.titlebar.ui.calendar
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.ini.titlebar.R
+import com.ini.titlebar.adapter.CalendarAdapter
 import com.ini.titlebar.databinding.FragmentCalendarBinding
+import com.ini.titlebar.model.CalendarVO
+import java.time.DayOfWeek
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.TemporalAdjusters
+import java.util.*
+import kotlin.collections.ArrayList
 
 class CalendarFragment : Fragment() {
 
     private var _binding: FragmentCalendarBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    lateinit var calendarAdapter: CalendarAdapter
+    private var calendarList = ArrayList<CalendarVO>()
+
+    companion object {
+        fun newInstance() = CalendarFragment()
     }
 
     override fun onCreateView(
@@ -25,4 +39,39 @@ class CalendarFragment : Fragment() {
 
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var week_day: Array<String> = resources.getStringArray(R.array.calendar_day)
+
+        calendarAdapter = CalendarAdapter(calendarList)
+
+        calendarList.apply {
+            val dateFormat = DateTimeFormatter.ofPattern("dd").withLocale(Locale.forLanguageTag("ko"))
+            val monthFormat = DateTimeFormatter.ofPattern("yyyy년 MM월").withLocale(Locale.forLanguageTag("ko"))
+
+            val localDate = LocalDateTime.now().format(monthFormat)
+            binding.textYearMonth.text = localDate
+
+            var preSunday: LocalDateTime = LocalDateTime.now().with(TemporalAdjusters.previous(DayOfWeek.SUNDAY))
+
+            for (i in 0..6) {
+                Log.d("날짜만", week_day[i])
+
+                calendarList.apply {
+                    add(CalendarVO(preSunday.plusDays(i.toLong()).format(dateFormat), week_day[i]))
+                }
+                Log.d("저번 주 일요일 기준으로 시작!", preSunday.plusDays(i.toLong()).format(dateFormat))
+            }
+        }
+        binding.weekRecycler.adapter = calendarAdapter
+        binding.weekRecycler.layoutManager = GridLayoutManager(context, 7)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
